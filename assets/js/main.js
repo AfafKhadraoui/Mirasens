b7144c44f134c1254923083105c7a75e82ff87b9
walaa-mansouri <218155652+walaa-mansouri@users.noreply.github.com>
2025-08-17 19:21:57 +0100

    new aurora theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1630,3 +1630,12 @@
   };
   eduJs.i();
 })(window, document, jQuery);
+ const starsContainer = document.querySelector('.stars-container');
+        for (let i = 0; i < 50; i++) {
+            const star = document.createElement('div');
+            star.classList.add('star');
+            star.style.top = `${Math.random() * 100}%`;
+            star.style.left = `${Math.random() * 100}%`;
+            star.style.animationDelay = `${Math.random() * 5}s`;
+            starsContainer.appendChild(star);
+        }
